8ad0107ef3c0d8d38f17138627cbc4bb9df4151a
João Alves <138532454+jonh213412421@users.noreply.github.com>
2024-03-28 13:51:56 -0300

    Update e-mail verifier.py

--- app with password autentication/e-mail verifier.py
+++ app with password autentication/e-mail verifier.py
@@ -1,5 +1,6 @@
 import email
 import imaplib
+import time
 from email.mime.multipart import MIMEMultipart
 import smtplib
 import socket
@@ -9,7 +10,7 @@ from email.mime.text import MIMEText
 def verify_reqs():
     requisitor = ''
     ipv6 = socket.getaddrinfo(socket.gethostname(), None, socket.AF_INET6)[2][4][0]
-    urls = []
+    requisition = []
     with open("e-mail.txt", "r") as f:
         usuario = f.readline()
         senha = f.readline()
@@ -23,44 +24,48 @@ def verify_reqs():
         raw = data[0][1]
         msg = email.message_from_bytes(raw)
         requisitor = msg['From']
+        mail.store(num, "+FLAGS", '\\Deleted')
         for part in msg.walk():
             if part.get_content_type() == "text/plain" or part.get_content_type() == "text/html":
                 message = part.get_payload(decode=True)
                 print("Message: \n", message.decode())
                 temp = message.split()
                 for tem in temp:
-                        urls.append(tem.decode('utf-8'))
-    print("requisição de ipconfig: ", requisitor)
-
-    for url in urls:
-        print(url)
-    smtp_servidor = "smtp.gmail.com"
-    smtp_porta = 587
-    mensagem = MIMEMultipart()
-    mensagem['From'] = usuario
-    mensagem['To'] = requisitor
-    mensagem['Subject'] = "Ipconfig"
-    corpo_email = f"Server ip: {ipv6}\n"
-    mensagem.attach(MIMEText(corpo_email, 'plain'))
-    try:
-        servidor = smtplib.SMTP(smtp_servidor, smtp_porta)
-        servidor.starttls()
-        servidor.login(usuario, senha)
-        servidor.sendmail(usuario, requisitor, mensagem.as_string())
-        servidor.quit()
-        print("Página enviada com sucesso!")
-        with open('log.txt', 'a') as f:
-            f.write(str(datetime.now().time().strftime("%H:%M:%S")))
-            f.write("-> ")
-            f.write(f"e-mail enviado para {requisitor} com sucesso!")
-            f.write("\n")
-    except Exception as e:
-        print(f"Erro ao enviar e-mail: {e}")
-        with open('log.txt', 'a') as f:
-            f.write(str(datetime.now().time().strftime("%H:%M:%S")))
-            f.write("-> ")
-            f.write(f"Erro. Falha em enviar endereço ip para {requisitor}")
-            f.write("\n")
+                    if tem.decode('utf-8') == 'ipconfig::':
+                        requisition.append(tem.decode('utf-8'))
+                        print("requisição de ipconfig: ", requisitor)
+        mail.expunge()
+    if len(requisition) != 0:
+        for url in requisition:
+            print(url)
+        smtp_servidor = "smtp.gmail.com"
+        smtp_porta = 587
+        mensagem = MIMEMultipart()
+        mensagem['From'] = usuario
+        mensagem['To'] = requisitor
+        mensagem['Subject'] = "server ip"
+        corpo_email = f"Server ip: {ipv6}\n"
+        mensagem.attach(MIMEText(corpo_email, 'plain'))
+        try:
+            servidor = smtplib.SMTP(smtp_servidor, smtp_porta)
+            servidor.starttls()
+            servidor.login(usuario, senha)
+            servidor.sendmail(usuario, requisitor, mensagem.as_string())
+            servidor.quit()
+            print("Página enviada com sucesso!")
+            with open('log.txt', 'a') as f:
+                f.write(str(datetime.now().time().strftime("%H:%M:%S")))
+                f.write("-> ")
+                f.write(f"e-mail enviado para {requisitor} com sucesso!")
+                f.write("\n")
+            time.sleep(5)
+        except Exception as e:
+            print(f"Erro ao enviar e-mail: {e}")
+            with open('log.txt', 'a') as f:
+                f.write(str(datetime.now().time().strftime("%H:%M:%S")))
+                f.write("-> ")
+                f.write(f"Erro. Falha em enviar endereço ip para {requisitor}")
+                f.write("\n")
 
 if __name__ == '__main__':
     while True:
