2d507939cafcb1067b4291f9509d62f224e87ca2
João Alves <138532454+jonh213412421@users.noreply.github.com>
2024-03-28 09:31:20 -0300

    Create e-mail verifies.py

---
+++ app with password autentication/e-mail verifies.py
@@ -0,0 +1,67 @@
+import email
+import imaplib
+from email.mime.multipart import MIMEMultipart
+import smtplib
+import socket
+from datetime import datetime
+from email.mime.text import MIMEText
+
+def verify_reqs():
+    requisitor = ''
+    ipv6 = socket.getaddrinfo(socket.gethostname(), None, socket.AF_INET6)[2][4][0]
+    urls = []
+    with open("e-mail.txt", "r") as f:
+        usuario = f.readline()
+        senha = f.readline()
+    imap_url = "imap.gmail.com"
+    mail = imaplib.IMAP4_SSL(imap_url)
+    mail.login(usuario, senha)
+    mail.select("INBOX")
+    _, req = mail.search(None, '(BODY "ipconfig::")')
+    for num in req[0].split():
+        resultado, data = mail.fetch(num, '(RFC822)')
+        raw = data[0][1]
+        msg = email.message_from_bytes(raw)
+        requisitor = msg['From']
+        for part in msg.walk():
+            if part.get_content_type() == "text/plain" or part.get_content_type() == "text/html":
+                message = part.get_payload(decode=True)
+                print("Message: \n", message.decode())
+                temp = message.split()
+                for tem in temp:
+                        urls.append(tem.decode('utf-8'))
+    print("requisição de ipconfig: ", requisitor)
+
+    for url in urls:
+        print(url)
+    smtp_servidor = "smtp.gmail.com"
+    smtp_porta = 587
+    mensagem = MIMEMultipart()
+    mensagem['From'] = usuario
+    mensagem['To'] = requisitor
+    mensagem['Subject'] = "Ipconfig"
+    corpo_email = f"Server ip: {ipv6}\n"
+    mensagem.attach(MIMEText(corpo_email, 'plain'))
+    try:
+        servidor = smtplib.SMTP(smtp_servidor, smtp_porta)
+        servidor.starttls()
+        servidor.login(usuario, senha)
+        servidor.sendmail(usuario, requisitor, mensagem.as_string())
+        servidor.quit()
+        print("Página enviada com sucesso!")
+        with open('log.txt', 'a') as f:
+            f.write(str(datetime.now().time().strftime("%H:%M:%S")))
+            f.write("-> ")
+            f.write(f"e-mail enviado para {requisitor} com sucesso!")
+            f.write("\n")
+    except Exception as e:
+        print(f"Erro ao enviar e-mail: {e}")
+        with open('log.txt', 'a') as f:
+            f.write(str(datetime.now().time().strftime("%H:%M:%S")))
+            f.write("-> ")
+            f.write(f"Erro. Falha em enviar endereço ip para {requisitor}")
+            f.write("\n")
+
+if __name__ == '__main__':
+    while True:
+        verify_reqs()
