4d8dfea2771c8f8950248edca3e5abbf74bdb3b5
João Alves <138532454+jonh213412421@users.noreply.github.com>
2024-12-08 18:17:10 -0300

    Update main.py

--- Desktop-server/main.py
+++ Desktop-server/main.py
@@ -1,23 +1,24 @@
+import time
+import qbittorrentapi
+import urllib.parse
 from flask import Flask, render_template, Response
 import requests
 import os
 import subprocess
+from pyngrok import ngrok
 from bs4 import BeautifulSoup
 import threading
 import base64
 import shutil
-import re
 
 app = Flask(__name__)
-
-# começa o ngrok. É necessário criar uma chave rsa no Termux e passar para o ngrok no seguinte link: https://dashboard.ngrok.com/ssh-keys
-# ajuste a porta no localhost:5000
-def ngrok():
-    subprocess.Popen(["ngrok", "http", "5000"])
+http_tunnel = ngrok.connect(5000)
+print(f"url:{http_tunnel.public_url}")
 
 # roda o app
 def run():
     app.run()
+
 
 # ajuda
 @app.route('/h')
@@ -25,16 +26,18 @@ def ajuda():
     ajuda = r"""
     Funções:<br><br>
     /math/path:expression retorna o resultado da expressão;<br>
-    /scrape/<path:url> retorna url;<br>
+    /nav/<path:url> retorna url;<br>
     /upload/<path:file> faz upload de arquivo local em hex;<br>
     /ls lista arquivos locais;<br>
     /tamanho/<path:link> retorna o tamanho do arquivo do link fornecido;<br>
     /download/<path:link> baixa o arquivo e retorna o hex dele;<br>
     /download_p/<path:start>/<path:end>/<path:link> baixa parte do arquivo e retorna o hex dele;<br>
     /download_torrent/<path:magnet> baixa torrent;<br>
+    /video_stream/<path:magnet> baixa filme da url fornecida e mostra na página;<br>
     /script_juntar retorna script para juntar arquivos.<br><br>
     """
     return ajuda
+
 
 # calculadora
 @app.route('/math/<path:expression>')
@@ -42,6 +45,7 @@ def math(expression):
     print(expression)
     resultado = f"Resultado: {eval(expression)}"
     return resultado
+
 
 # script para juntar arquivos. Testar
 @app.route('/script_juntar')
@@ -55,15 +59,16 @@ def juntar():
         with open(r"preencher com o caminho do arquivo de saída", "wb") as f:
             f.write(bin)
             f.close()
-            
+
         cmd: 
         certutil -decodehex test.txt output.pdf se hex
         certutil -decode test.txt output.pdf se bs64
     """
     return juntar
 
+
 # Rota para scrape. Tentar usar curl no futuro
-@app.route('/scrape/<path:url>')
+@app.route('/nav/<path:url>')
 def scrape_page(url):
     url = "https://" + url
     try:
@@ -72,6 +77,7 @@ def scrape_page(url):
         return str(soup)
     except Exception as e:
         return e
+
 
 # faz upload
 @app.route('/upload/<path:file>')
@@ -82,11 +88,13 @@ def upload(file):
         f.close()
     return bas64
 
+
 # show video located in static
 @app.route('/video/<path:url>')
 def video(url):
     video_path = url
     return render_template("index2.html", video_path=video_path)
+
 
 # lista dir
 @app.route('/ls')
@@ -97,14 +105,16 @@ def ls():
         arquivos = arquivos + arquivo + "<br>"
     return arquivos
 
-#pega tamanho de arquivos
+
+# pega tamanho de arquivos
 @app.route('/tamanho/<path:link>')
 def tamanho(link):
     tamanho = requests.head(link).headers.get('Content-Length')
     tamanho = str(tamanho) + " bytes"
     return tamanho
 
-#baixa arquivos e envia o hex
+
+# baixa arquivos e envia o hex
 @app.route('/download/<path:link>')
 def download(link):
     nome = str(link).split("/")[-1]
@@ -116,7 +126,8 @@ def download(link):
     conteudo = base64.b64encode(conteudo)
     return conteudo
 
-#baixa porção de arquivo e envia hex
+
+# baixa porção de arquivo e envia hex
 @app.route('/download_p/<path:start>/<path:end>/<path:link>')
 def dowload_p(link, start, end):
     nome = str(link).split("/")[-1]
@@ -130,7 +141,8 @@ def dowload_p(link, start, end):
     conteudo = base64.b64encode(conteudo)
     return conteudo
 
-#função utilizada no download de torrents
+
+# função utilizada no download de torrents
 def dt(magnet):
     magnet = "magnet:?" + magnet
     processo = subprocess.Popen(
@@ -154,17 +166,67 @@ def dt(magnet):
             print(chunk_data)
             conteudo = base64.b64encode(chunk_data)
             yield conteudo
-            
-#baixa torrent - passar magnet
+
+
+# baixa torrent - passar magnet
 @app.route('/donwload_torrent/<path:magnet>')
 def download_torrent(magnet):
-    return Response(dt(magnet),mimetype='text/plain')
+    return Response(dt(magnet), mimetype='text/plain')
+
+@app.route('/video_stream/<path:magnet>')
+def video_dw(magnet):
+    magnet = "magnet:?xt=" + magnet
+    conn_info = dict(
+        host="localhost",
+        port=8080,
+        username="admin",
+        password="123321",
+    )
+    qbt_client = qbittorrentapi.Client(**conn_info)
+
+    try:
+        qbt_client.auth_log_in()
+    except qbittorrentapi.LoginFailed as e:
+        print(e)
+
+    qbt_client.torrents.add(magnet, is_sequential_download=True, download_path=os.path.join(os.getcwd(), "static"), save_path=os.path.join(os.getcwd(), "static"))
+    a = qbt_client.torrents.info()
+    for b in a:
+        c = urllib.parse.parse_qs(urllib.parse.urlparse(b['magnet_uri']).query).get('dn', [None])[0]
+        d = urllib.parse.parse_qs(urllib.parse.urlparse(magnet).query).get('dn', [None])[0]
+        if c == d:
+            path = b['content_path']
+            print(path)
+            files = os.listdir(path)
+            for file in files:
+                if file.endswith(".mp4") or file.endswith(".avi"):
+                    video = os.path.basename(path) + "/" + file
+                    print(video)
+            #https://en.yts-official.mx/movies
+            return render_template('index2.html', video_path=video)
+
+def tor_status():
+    conn_info = dict(
+        host="localhost",
+        port=8080,
+        username="admin",
+        password="123321",
+    )
+
+    qbt_client = qbittorrentapi.Client(**conn_info)
+    a = qbt_client.torrents.info()
+    while True:
+        for tor in a:
+            yield f"data: {tor['name']} - {tor['progress']*100:.2f}%\n\n"
+            time.sleep(5)
+
+@app.route('/torrent_status')
+def torrent_progress():
+    return Response(tor_status(), content_type='text/event-stream')
 
 if __name__ == '__main__':
     # múltiplas threads
-    thread1 = threading.Thread(target=ngrok)
-    thread1.start()
-    thread2 = threading.Thread(target=run)
-    thread2.start()
-    thread1.join()
-    thread2.join()
+    thread = threading.Thread(target=run)
+    thread.start()
+    thread.join()
+    
